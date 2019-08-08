6f91b5110cc0278ecaebe32028d792eb17375caa
ntvnps32 <psrinath@thegeneral.com>
2019-08-08 18:43:47 +0530

    third commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreated = false;
   serverName = '';
+  servers = ['server1'];
+
   constructor() {
     setTimeout(()=> {
       this.allowNewServer = true;
@@ -19,6 +21,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
   handleChange(e){
